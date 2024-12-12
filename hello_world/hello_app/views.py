051bca100fba28540b4306bcd17c0edecde6ccb6
Artem Aslyamov <aaslyamov.arts@gmail.com>
2024-12-12 17:26:19 +0200

    homework

--- hello_world/hello_app/views.py
+++ hello_world/hello_app/views.py
@@ -1,4 +1,12 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+
+from .models import User
 
 def hello_world(request):
     return HttpResponse("Привіт, світ!")
+
+
+def user_list(request):
+    users = User.objects.all()
+    return render(request, 'hello_app/user_list.html', {'users': users})
--- hello_world/hello_world/urls.py
+++ hello_world/hello_world/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path
-from hello_app.views import hello_world
+from hello_app.views import hello_world, user_list
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', hello_world, name='hello_world'),
+    path('users/', user_list, name='user_list'),
 ]
